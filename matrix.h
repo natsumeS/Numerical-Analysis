ee178f5a672a3f87daa3f52d6a94566271d742d2
natsumeS <natsumeshun.kyoto@gmail.com>
2017-12-15 18:52:01 +0900

    inner
    struct->double

--- matrix.h
+++ matrix.h
@@ -2,20 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct{
-	int dim;
-	double *element;
-} Vector;
-typedef struct{
-	int row;
-	int column;
-	double *element;
-} Matrix,SQMatrix;
-
-void createVector(Vector *v,double *e,int dim);
-void createMatrix(Matrix *m,double *e,int row,int column);
-void createSQMatrix(Matrix *m,double *e,int dim);
-double inner(Vector *a,Vector *b);
+double inner(double *a,double *b,int dim);
 void MVprod(Matrix *M,Vector *v,Vector *output);
 double norm2(Vector *v1,Vector *v2);
 void GaussianEliminate(Matrix *A,Vector *b);
